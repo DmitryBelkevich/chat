db865d077d18498accf06ba11dd7d7b45ee44b3b
hard <harleydav@mail.ru>
2018-06-11 22:36:34 +0300

    service

---
+++ webapp/src/main/java/com/hard/services/EntityService.java
@@ -0,0 +1,10 @@
+package com.hard.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EntityService {
+    public String getEntity() {
+        return null;
+    }
+}
