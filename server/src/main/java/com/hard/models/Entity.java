6d0368f48360dda7831d326d4dc9f890a4b49890
hard <harleydav@mail.ru>
2018-06-11 22:07:48 +0300

    server

---
+++ server/src/main/java/com/hard/models/Entity.java
@@ -0,0 +1,16 @@
+package com.hard.models;
+
+public class Entity {
+    public int x;
+    public int y;
+
+    public int w;
+    public int h;
+
+    public Entity(int x, int y, int w, int h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+}
