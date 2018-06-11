0204c40da5950050e505c7e01a98839822200b80
hard <harleydav@mail.ru>
2018-06-12 01:28:36 +0300

    desktop client

--- desktop-app/src/main/java/com/hard/Client.java
+++ desktop-app/src/main/java/com/hard/Client.java
@@ -1,22 +1,57 @@
 package com.hard;
 
-import javax.swing.*;
-import java.awt.*;
+import com.hard.views.View;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
 
 public class Client {
+    private View view = new View();
+
+    private String host = "18.219.167.139";
+    private int port = 9999;
+    private Socket socket = null;
+
     public void run() {
-        JFrame frame = new JFrame("Client");
+        view.run();
 
-        frame.setSize(640, 480);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
+        init();
+        String str = readData();
+        System.out.println(str);
+    }
 
-        JPanel panel = new JPanel();
-        panel.setPreferredSize(new Dimension(640, 480));
-        frame.add(panel);
+    public void init() {
+        try {
+            socket = new Socket(host, port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-        frame.pack();
-        frame.setVisible(true);
+    public String readData() {
+        InputStream inputStream = null;
+        try {
+            inputStream = socket.getInputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        DataInputStream dataInputStream = new DataInputStream(inputStream);
+        String result = null;
+        try {
+            result = dataInputStream.readUTF();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
