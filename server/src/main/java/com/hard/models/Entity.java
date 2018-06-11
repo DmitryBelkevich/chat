d737f3d53fbccc9ba07e1f9ee9da4e15fcb1347c
hard <harleydav@mail.ru>
2018-06-11 23:02:01 +0300

    refactoring

--- server/src/main/java/com/hard/models/Entity.java
+++ server/src/main/java/com/hard/models/Entity.java
@@ -1,16 +1,26 @@
 package com.hard.models;
 
 public class Entity {
-    public int x;
-    public int y;
+    private int x;
+    private int y;
 
-    public int w;
-    public int h;
+    private int w;
+    private int h;
 
     public Entity(int x, int y, int w, int h) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
+    }
+
+    @Override
+    public String toString() {
+        return "Entity{" +
+                "x=" + x +
+                ", y=" + y +
+                ", w=" + w +
+                ", h=" + h +
+                '}';
     }
 }
