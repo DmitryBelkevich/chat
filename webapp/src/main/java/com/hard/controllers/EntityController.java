145b1f5a378e6fff45a00bee4fc51b78034cbed2
hard <harleydav@mail.ru>
2018-06-12 00:45:34 +0300

    entity controller

---
+++ webapp/src/main/java/com/hard/controllers/EntityController.java
@@ -0,0 +1,22 @@
+package com.hard.controllers;
+
+import com.hard.services.EntityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/entities")
+public class EntityController {
+    @Autowired
+    private EntityService entityService;
+
+    @GetMapping(value = "")
+    public String get() {
+        String entity = entityService.getEntity();
+        System.out.println(entity);
+
+        return "main";
+    }
+}
