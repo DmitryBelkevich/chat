1fdbfdf119a33ee87588297970fa5ae82371b959
hard <harleydav@mail.ru>
2018-06-13 00:02:14 +0300

    refactoring

---
+++ server/src/main/java/com/hard/Client.java
@@ -0,0 +1,94 @@
+package com.hard;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Client implements Runnable {
+    private Server server;
+
+    private Socket socket;
+    private InputStream inputStream;
+    private OutputStream outputStream;
+
+    public Client(Server server, Socket socket) {
+        this.server = server;
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        initStreams();
+
+        while (true) {
+            String str = read();
+
+            System.out.println("[" + this + "]:" + str);
+
+            if (str.equalsIgnoreCase("/exit"))
+                break;
+
+            server.notifyAllClients("[" + this + "]:" + str);
+        }
+
+        stop();
+    }
+
+    private void initStreams() {
+        try {
+            inputStream = socket.getInputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            outputStream = socket.getOutputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stop() {
+        try {
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        server.remove(this);
+    }
+
+    private String read() {
+        DataInputStream dataInputStream = new DataInputStream(inputStream);
+        String result = null;
+        try {
+            result = dataInputStream.readUTF();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    public void write(String str) {
+        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+        try {
+            dataOutputStream.writeUTF(str);
+            dataOutputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+            stop();
+        }
+    }
+}
