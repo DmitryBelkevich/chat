0186360e8838ef4fd25405d5bf4c68116104116d
hard <harleydav@mail.ru>
2018-06-21 06:11:56 +0300

    refactoring

--- client/desktop-app/src/main/java/com/hard/DesktopClientLoader.java
+++ client/desktop-app/src/main/java/com/hard/DesktopClientLoader.java
@@ -2,15 +2,10 @@ package com.hard;
 
 import com.hard.controllers.ClientController;
 import com.hard.models.Client;
-import com.hard.views.View;
-
-import java.util.ArrayList;
-import java.util.Collection;
 
 public class DesktopClientLoader {
     public static void main(String[] args) {
         Client client = new Client();
-        Collection<View> views = new ArrayList<>();
 
         ClientController clientController = new ClientController(client);
 
